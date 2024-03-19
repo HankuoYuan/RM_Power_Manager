857842f798989a7bd14f731f23aee38b22aac41f
xiaokaizip <927639561qq.com>
2024-03-19 21:02:43 +0800

    Update Referee 1.6.1 form 1.4

--- algorithm/referee.c
+++ algorithm/referee.c
@@ -3,10 +3,10 @@
 //
 
 #include "referee.h"
+#include "dma.h"
+#include "string.h"
 #include "system.h"
 #include "verify.h"
-#include "string.h"
-#include "dma.h"
 
 typedef struct {
   unsigned char dummy_byte;
@@ -22,7 +22,8 @@ typedef struct {
   unsigned char crc16_low;
 } referee_package_t;
 
-static unsigned char referee_process_status = 0, referee_header_buffer[5] = {0xA5, [1 ... 4]=0};
+static unsigned char referee_process_status = 0,
+                     referee_header_buffer[5] = {0xA5, [1 ... 4] = 0};
 static referee_package_t current_package = {0};
 volatile referee_info_t referee_info;
 
@@ -33,12 +34,12 @@ const unsigned short k_game_robot_status_cmd = 0x0201;
 const unsigned short k_power_heat_data_cmd = 0x0202;
 
 const unsigned int k_game_status_size = 11;
-const unsigned int k_game_robot_status_size = 27;
+const unsigned int k_game_robot_status_size = 13;
 const unsigned int k_power_heat_data_size = 16;
 
 static struct GameStatus_t {
-  uint8_t game_type: 4;
-  uint8_t game_progress: 4;
+  uint8_t game_type : 4;
+  uint8_t game_progress : 4;
   uint16_t stage_remain_time;
   uint64_t time_stamp;
 } game_status;
@@ -48,19 +49,12 @@ static struct GameRobotStatus_t {
   uint8_t robot_level;
   uint16_t remain_hp;
   uint16_t max_hp;
-  uint16_t shooter_id1_17mm_cooling_rate;
-  uint16_t shooter_id1_17mm_cooling_limit;
-  uint16_t shooter_id1_17mm_speed_limit;
-  uint16_t shooter_id2_17mm_cooling_rate;
-  uint16_t shooter_id2_17mm_cooling_limit;
-  uint16_t shooter_id2_17mm_speed_limit;
-  uint16_t shooter_id1_42mm_cooling_rate;
-  uint16_t shooter_id1_42mm_cooling_limit;
-  uint16_t shooter_id1_42mm_speed_limit;
+  uint16_t shooter_barrel_cooling_value;
+  uint16_t shooter_barrel_heat_limit;
   uint16_t chassis_power_limit;
-  uint8_t mains_power_gimbal_output: 1;
-  uint8_t mains_power_chassis_output: 1;
-  uint8_t mains_power_shooter_output: 1;
+  uint8_t mains_power_gimbal_output : 1;
+  uint8_t mains_power_chassis_output : 1;
+  uint8_t mains_power_shooter_output : 1;
 } game_robot_status;
 
 static struct PowerHeatData_t {
@@ -75,100 +69,116 @@ static struct PowerHeatData_t {
 
 static unsigned int last_time_tick = 0;
 unsigned int referee_available(void) {
-    return ((HAL_GetTick() - last_time_tick) < 200);
+  return ((HAL_GetTick() - last_time_tick) < 200);
 }
 
 void referee_process_buffer(const unsigned char *buffer) {
-    int frame_header_position = -1;
-    unsigned short crc16_expected;
-    for (int counter = 0; counter < REFEREE_DMA_BUFFER_SIZE; ++counter) {
-        switch (referee_process_status) {
-            case 0:if (buffer[counter] == 0xA5) referee_process_status = 1;
-                break;
-            case 1:current_package.header.data_length = buffer[counter];
-                referee_header_buffer[1] = buffer[counter];
-                referee_process_status = 2;
-                break;
-            case 2:current_package.header.data_length |= (buffer[counter] << 8);
-                current_package.header.data_length = current_package.header.data_length;
-                referee_header_buffer[2] = buffer[counter];
-                referee_process_status = 3;
-                break;
-            case 3:current_package.header.package_sequence = buffer[counter];
-                referee_header_buffer[3] = buffer[counter];
-                referee_process_status = 4;
-                break;
-            case 4:
-                if (get_crc8_value(referee_header_buffer, 4) == buffer[counter]) {
-                    referee_process_status = 5;
-                    frame_header_position = counter - 4;
-                    referee_header_buffer[4] = buffer[counter];
-                } else
-                    goto clear_all_referee_data;
-                break;
-            case 5:current_package.cmd_id = buffer[counter];
-                referee_process_status = 6;
-                break;
-            case 6:current_package.cmd_id |= (buffer[counter] << 8);
-                current_package.data_counter = 0;
-                referee_process_status = 7;
-                if (current_package.cmd_id != k_game_robot_status_cmd &&
-                    current_package.cmd_id != k_game_status_cmd &&
-                    current_package.cmd_id != k_power_heat_data_cmd)
-                    referee_process_status = 10;
-                break;
-            case 7:current_package.data[current_package.data_counter] = buffer[counter];
-                current_package.data_counter++;
-                if (current_package.data_counter == current_package.header.data_length)
-                    referee_process_status = 8;
-                break;
-            case 8:current_package.crc16_low = buffer[counter];
-                referee_process_status = 9;
-                break;
-            case 9:
-                crc16_expected = get_crc16_value(referee_header_buffer,
-                                                 current_package.cmd_id,
-                                                 current_package.data,
-                                                 current_package.header.data_length);
-                if ((crc16_expected & 0xff) == current_package.crc16_low
-                    && ((crc16_expected >> 8) & 0xff) == buffer[counter])
-                    referee_process_struct(&current_package);
-                goto clear_all_referee_data;
-            case 10:current_package.data_counter++;
-                if (current_package.data_counter == current_package.header.data_length + 2)
-                    goto clear_all_referee_data;
-                break;
-            default:
-            clear_all_referee_data:
-                referee_process_status = 0;
-                frame_header_position = -1;
-                memset(&referee_header_buffer[1], 0x00, 4);
-                memset(&current_package, 0x00, sizeof(referee_package_t));
-                break;
-        }
+  int frame_header_position = -1;
+  unsigned short crc16_expected;
+  for (int counter = 0; counter < REFEREE_DMA_BUFFER_SIZE; ++counter) {
+    switch (referee_process_status) {
+    case 0:
+      if (buffer[counter] == 0xA5)
+        referee_process_status = 1;
+      break;
+    case 1:
+      current_package.header.data_length = buffer[counter];
+      referee_header_buffer[1] = buffer[counter];
+      referee_process_status = 2;
+      break;
+    case 2:
+      current_package.header.data_length |= (buffer[counter] << 8);
+      current_package.header.data_length = current_package.header.data_length;
+      referee_header_buffer[2] = buffer[counter];
+      referee_process_status = 3;
+      break;
+    case 3:
+      current_package.header.package_sequence = buffer[counter];
+      referee_header_buffer[3] = buffer[counter];
+      referee_process_status = 4;
+      break;
+    case 4:
+      if (get_crc8_value(referee_header_buffer, 4) == buffer[counter]) {
+        referee_process_status = 5;
+        frame_header_position = counter - 4;
+        referee_header_buffer[4] = buffer[counter];
+      } else
+        goto clear_all_referee_data;
+      break;
+    case 5:
+      current_package.cmd_id = buffer[counter];
+      referee_process_status = 6;
+      break;
+    case 6:
+      current_package.cmd_id |= (buffer[counter] << 8);
+      current_package.data_counter = 0;
+      referee_process_status = 7;
+      if (current_package.cmd_id != k_game_robot_status_cmd &&
+          current_package.cmd_id != k_game_status_cmd &&
+          current_package.cmd_id != k_power_heat_data_cmd)
+        referee_process_status = 10;
+      break;
+    case 7:
+      current_package.data[current_package.data_counter] = buffer[counter];
+      current_package.data_counter++;
+      if (current_package.data_counter == current_package.header.data_length)
+        referee_process_status = 8;
+      break;
+    case 8:
+      current_package.crc16_low = buffer[counter];
+      referee_process_status = 9;
+      break;
+    case 9:
+      crc16_expected = get_crc16_value(
+          referee_header_buffer, current_package.cmd_id, current_package.data,
+          current_package.header.data_length);
+      if ((crc16_expected & 0xff) == current_package.crc16_low &&
+          ((crc16_expected >> 8) & 0xff) == buffer[counter])
+        referee_process_struct(&current_package);
+      goto clear_all_referee_data;
+    case 10:
+      current_package.data_counter++;
+      if (current_package.data_counter ==
+          current_package.header.data_length + 2)
+        goto clear_all_referee_data;
+      break;
+    default:
+    clear_all_referee_data:
+      referee_process_status = 0;
+      frame_header_position = -1;
+      memset(&referee_header_buffer[1], 0x00, 4);
+      memset(&current_package, 0x00, sizeof(referee_package_t));
+      break;
     }
-    mdma_transmit_buffer(buffer, frame_header_position);
+  }
+  mdma_transmit_buffer(buffer, frame_header_position);
 }
 
 static void referee_process_struct(referee_package_t *package) {
-    switch (package->cmd_id) {
-        case k_game_status_cmd:memcpy(&game_status, package->data, k_game_status_size);
-            referee_info.game_progress = game_status.game_progress;
-            referee_info.timestamp = game_status.time_stamp;
-            break;
-        case k_game_robot_status_cmd:memcpy(&game_robot_status, package->data, k_game_robot_status_size);
-            if (game_robot_status.chassis_power_limit <= 240)
-                referee_info.chassis_power_limit = game_robot_status.chassis_power_limit;
-            else
-                referee_info.chassis_power_limit = 80;
-            break;
-        case k_power_heat_data_cmd:memcpy(&power_heat_data, package->data, k_power_heat_data_size);
-            referee_info.chassis_power = power_heat_data.chassis_power;
-            referee_info.chassis_voltage = (float) power_heat_data.chassis_volt / 1000.0f;
-            referee_info.chassis_current = (float) power_heat_data.chassis_current / 1000.0f;
-            referee_info.chassis_power_buffer = power_heat_data.chassis_power_buffer;
-            break;
-        default:break;
-    }
-    last_time_tick = HAL_GetTick();
+  switch (package->cmd_id) {
+  case k_game_status_cmd:
+    memcpy(&game_status, package->data, k_game_status_size);
+    referee_info.game_progress = game_status.game_progress;
+    referee_info.timestamp = game_status.time_stamp;
+    break;
+  case k_game_robot_status_cmd:
+    memcpy(&game_robot_status, package->data, k_game_robot_status_size);
+    if (game_robot_status.chassis_power_limit <= 240)
+      referee_info.chassis_power_limit = game_robot_status.chassis_power_limit;
+    else
+      referee_info.chassis_power_limit = 80;
+    break;
+  case k_power_heat_data_cmd:
+    memcpy(&power_heat_data, package->data, k_power_heat_data_size);
+    referee_info.chassis_power = power_heat_data.chassis_power;
+    referee_info.chassis_voltage =
+        (float)power_heat_data.chassis_volt / 1000.0f;
+    referee_info.chassis_current =
+        (float)power_heat_data.chassis_current / 1000.0f;
+    referee_info.chassis_power_buffer = power_heat_data.chassis_power_buffer;
+    break;
+  default:
+    break;
+  }
+  last_time_tick = HAL_GetTick();
 }
