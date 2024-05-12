4b9675d75ee5b8ce2a0539d425a515d22f35ef91
xiaokaizip <927639561qq.com>
2024-05-12 15:39:13 +0800

    Add main.h file to fsm

--- tasks/fsm.h
+++ tasks/fsm.h
@@ -5,27 +5,32 @@
 #ifndef POWER_MANAGER_TASKS_FSM_H_
 #define POWER_MANAGER_TASKS_FSM_H_
 
+#include "main.h"
+
 typedef enum {
-  charge_mode = 0,
-  boost_mode = 1,
-  normal_mode = 2,
-  all_off_mode = 3,
-  selftest_mode = 4,
-  refresh_mode = 5,
+    charge_mode = 0,
+    boost_mode = 1,
+    normal_mode = 2,
+    all_off_mode = 3,
+    selftest_mode = 4,
+    refresh_mode = 5,
 } mode_target_e;
 
 typedef enum {
-  pass_through = 0,
-  charge_with_boost = 1,
-  switches_all_off = 2,
-  refresh_typology = 3
+    pass_through = 0,
+    charge_with_boost = 1,
+    switches_all_off = 2,
+    refresh_typology = 3
 } typology_e;
 
 extern TaskHandle_t fsm_task_handler;
 
 void fsm_set_mode(mode_target_e target_mode);
+
 mode_target_e fsm_get_mode(void);
+
 typology_e fsm_get_typology(void);
+
 _Noreturn void fsm_task(void *parameters);
 
-#endif  // POWER_MANAGER_TASKS_FSM_H_
+#endif // POWER_MANAGER_TASKS_FSM_H_
